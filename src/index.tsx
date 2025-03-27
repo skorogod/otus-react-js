f19525ab4e59a4d76c2a0ce6cd3bf71935d7361c
sergey <skorogod123@yandex.ru>
2025-03-28 01:59:12 +0300

    fix: change router to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./app/index.css";
 import App from "./app/App";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { ProfileScreen } from "./pages/profile/profileScreen/ProfileScreen";
 import { ProductsScreen } from "./pages/products/productsScreen/ProductsScreen";
 import { ProductCartScreen } from "./pages/ProductCartScreen/ProductCartScreen";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
